4486610caf8b8483ff730aa71137d6e056935633
Vishwas Puri <vishwaspuriofficial@gmail.com>
2023-04-27 21:23:17 -0400

    Updated and Fixed Error in Simulation

--- Website/assets/js/simulation.js
+++ Website/assets/js/simulation.js
@@ -2,8 +2,6 @@ var SimulationArea = {
   canvas: document.getElementById("myCanvas"),
   start: function() {
     this.delay = 1000;
-    this.canvas.width = 500;
-    this.canvas.height = 250;
     this.context = this.canvas.getContext("2d");
   },
   clear: function() {
@@ -14,26 +12,32 @@ var SimulationArea = {
 function Vector(x, y) {
   this.x = x;
   this.y = y;
+  this.add = function(x2, y2) {
+    this.x = this.x + x2;
+    this.y = this.y + y2;
+  }
 }
 
-function RigidBody(width, height, position, velocity, acceleration) {
+class RigidBody {
+  constructor(width, height, position, velocity, acceleration) {
   this.width = width;
   this.height = height;
   this.position = new Vector(...position);
   this.velocity = new Vector(...velocity);
-  this.acceleration = new Vector(...acceleration);
-}
+  this.acceleration = new Vector(...acceleration); 
+  this.ctx = SimulationArea.context;}
 
-function update(body) {
-  body.ctx = SimulationArea.context;
-  ctx.fillRect(body.position.x, body.position.y, body.width, body.height);
-  body.position.x += body.velocity.x
-  body.position.y += body.velocity.y
+  update() {
+    this.ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+    this.position.add(this.velocity.x, this.velocity.y)
+  }
 }
 
 SimulationArea.start();
-var ctx = SimulationArea.context;
 
 bodies = [];
 bodies.push(new RigidBody(100, 10, [100, 20], [10, 10], [0, 0]));
-setInterval(() => {update(bodies[0]);}, SimulationArea.delay);
+setInterval(function () {
+  SimulationArea.clear();
+  bodies[0].update();
+}, 1000);
