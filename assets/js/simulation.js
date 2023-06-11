8d20b13790143b30b6ec348acdb4a2e470f194d7
Vishwas Puri <vishwaspuriofficial@gmail.com>
2023-06-11 02:33:15 -0400

    Updated code to create graphs after the simulation ends to avoid lag

--- assets/js/simulation.js
+++ assets/js/simulation.js
@@ -21,11 +21,11 @@ render.options.wireframes = false;
 
 // create two boxes and a 4ground
 var boxA = Bodies.rectangle(400, 200, 80, 80);
-// var boxB = Bodies.rectangle(450, 50, 80, 80);
+var boxB = Bodies.rectangle(450, 50, 80, 80);
 var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
 
 // add all of the bodies to the world
-Composite.add(engine.world, [boxA, ground]);
+Composite.add(engine.world, [boxA, boxB, ground]);
 
 // run the renderer
 Render.run(render);
@@ -45,28 +45,29 @@ accelerationX = [];
 accelerationY = [];
 time = [0];
 
-
+//Dynamic Frame Per Second
+fps = 100
 
 
 var timer = setInterval(() => {
     if ((positionX.at(-1) == boxA.position.x) && (positionY.at(-1) == boxA.position.y)) {
+        xptGraph();
+        yptGraph();
+        xvtGraph();
+        yvtGraph();
+        xatGraph();
+        yatGraph();
         clearInterval(timer);
     }
     positionX.push(boxA.position.x);
     positionY.push(boxA.position.y);
     velocityX.push(boxA.velocity.x);
     velocityY.push(boxA.velocity.y);
-    accelerationX.push((velocityX.at(-1)-velocityX.at(-2))/0.1);
-    accelerationY.push((velocityY.at(-1)-velocityY.at(-2))/0.1);
+    accelerationX.push((velocityX.at(-1)-velocityX.at(-2))/(fps/1000));
+    accelerationY.push((velocityY.at(-1)-velocityY.at(-2))/(fps/1000));
     time.push(Math.round((time.at(-1)+0.1) * 10) / 10);
     console.log(time.at(-1))
-    xptGraph();
-    yptGraph();
-    xvtGraph();
-    yvtGraph();
-    xatGraph();
-    yatGraph();
-}, 100)
+}, fps)
 
 function xptGraph() {
     new Chart("xptGraph", {
@@ -157,3 +158,4 @@ function yatGraph() {
         options: {}
       });
 }
+
