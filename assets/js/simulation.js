d41d9030e151eacd99c27e229cde5fc43dd46f16
Vishwas Puri <vishwaspuriofficial@gmail.com>
2023-06-12 04:05:57 -0400

    Updated simulation

--- assets/js/simulation.js
+++ assets/js/simulation.js
@@ -53,7 +53,7 @@ Composite.add(engine.world, [ground]);
 Render.run(render);
 
 // create runner
-var runner = Runner.create({isFixed: true});
+var runner = Runner.create();
 
 // run the engine
 Runner.run(runner, engine);
@@ -63,13 +63,13 @@ fps = 1
 objects = []
 vals = []
 letters = " ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-colours = ["red", "orange", "yellow", "green", "blue", "purple"]
+colours = ["Red", "Orange", "Yellow", "Green", "Blue", "Purple"]
 
 
 function startSimulation() {
     objects.forEach(function(object) {
         Matter.Body.setStatic(object, false)
-        simulationData()
+        // simulationData()
     });
 }
 
@@ -104,36 +104,38 @@ function dynamicOptions() {
  document.getElementById("dynamicObjects").innerHTML = options;
 }
 
-function simulationData() {
-    objects.forEach(function(object){
-        console.log(object)
-        Object.assign(object,{positionX: [object.position.x]});
-        Object.assign(object,{positionY: [object.position.y]});
-        Object.assign(object,{velocityX: [object.velocity.x]});
-        Object.assign(object,{velocityY: [object.velocity.x]});
-        Object.assign(object,{accelerationX: []});
-        Object.assign(object,{accelerationY: []});
-        Object.assign(object,{time: [0]});
 
-        Math.round((object.positionX.at(-1)) * 100) / 100
 
-        var timer = setInterval(() => {
-            console.log(object.positionY.at(-1), object.position.y);
-            if (((Math.round((object.positionX.at(-1)) * 100) / 100) == (Math.round((object.position.x) * 100) / 100)) && ((Math.round((object.positionY.at(-1)) * 100) / 100) == (Math.round((object.position.y) * 100) / 100))) {
-                vals.push(object)
-                clearInterval(timer); 
-                console.log(vals);
-            }
-            object.positionX.push(object.position.x);
-            object.positionY.push(object.position.y);
-            object.velocityX.push(object.velocity.x);
-            object.velocityY.push(object.velocity.y);
-            object.accelerationX.push((object.velocityX.at(-1)-object.velocityX.at(-2))/(fps/1000));
-            object.accelerationY.push((object.velocityY.at(-1)-object.velocityY.at(-2))/(fps/1000));
-            object.time.push(Math.round((object.time.at(-1)+0.1) * 10) / 10);
-        }, fps)
-    });
-}
+// function simulationData() {
+//     objects.forEach(function(object){
+//         console.log(object)
+//         Object.assign(object,{positionX: [object.position.x]});
+//         Object.assign(object,{positionY: [object.position.y]});
+//         Object.assign(object,{velocityX: [object.velocity.x]});
+//         Object.assign(object,{velocityY: [object.velocity.x]});
+//         Object.assign(object,{accelerationX: []});
+//         Object.assign(object,{accelerationY: []});
+//         Object.assign(object,{time: [0]});
+
+//         Math.round((object.positionX.at(-1)) * 100) / 100
+
+//         var timer = setInterval(() => {
+//             console.log(object.positionY.at(-1), object.position.y);
+//             if (((Math.round((object.positionX.at(-1)) * 100) / 100) == (Math.round((object.position.x) * 100) / 100)) && ((Math.round((object.positionY.at(-1)) * 100) / 100) == (Math.round((object.position.y) * 100) / 100))) {
+//                 vals.push(object)
+//                 clearInterval(timer); 
+//                 console.log(vals);
+//             }
+//             object.positionX.push(object.position.x);
+//             object.positionY.push(object.position.y);
+//             object.velocityX.push(object.velocity.x);
+//             object.velocityY.push(object.velocity.y);
+//             object.accelerationX.push((object.velocityX.at(-1)-object.velocityX.at(-2))/(fps/1000));
+//             object.accelerationY.push((object.velocityY.at(-1)-object.velocityY.at(-2))/(fps/1000));
+//             object.time.push(Math.round((object.time.at(-1)+0.1) * 10) / 10);
+//         }, fps)
+//     });
+// }
 
 function showObjects(graph) {
     document.getElementById("graph").innerHTML = "Choose Graph: " + graph;
@@ -154,8 +156,7 @@ function createGraph(selectedObject, graph) {
     document.getElementById("xat").hidden = true;
     document.getElementById("yat").hidden = true;
     document.getElementById(graph).hidden = false;
-    object = objects.at(objects.indexOf(selectedObject));
-    // console.log(objects[-1],objects.indexOf(selectedObject), selectedObject)
+    object = objects.at(colours.indexOf(selectedObject));
     if (graph=="xpt") {
         drawGraph(object, object.positionX, "xptGraph")
     }
@@ -195,151 +196,3 @@ function selectObject(obj) {
     document.getElementById("obj").innerHTML = "Choose Object: " + obj;
     createGraph(obj, document.getElementById("graph").innerText.slice(-3))
 }
-
-// document.getElementById("simulation").onclick = function(e) {
-//     console.log(e.offsetX,e.offsetY)
-//     objects.at(-1).x = e.offsetX
-//     objects.at(-1).y = e.offsetY
-//     if (count==0) {
-//         const img = new Image(50, 50);
-//         img.id = "A";
-//         img.src = "A.png"; 
-//         ay = e.offsetY-50
-//         ax = e.offsetX-25
-//         img.style=`position: absolute; top:${ay}px; left:${ax}px`;
-//         document.body.appendChild(img);
-//     }
-
-
-// positionX = [boxA.position.x];
-// positionY = [boxA.position.y];
-// velocityX = [boxA.velocity.x];
-// velocityY = [boxA.velocity.y];
-// accelerationX = [];
-// accelerationY = [];
-// time = [0];
-
-// //Dynamic Frame Per Second
-// fps = 0.01
-
-
-// var timer = setInterval(() => {
-//     if ((positionX.at(-1) == boxA.position.x) && (positionY.at(-1) == boxA.position.y)) {
-//         xptGraph();
-//         yptGraph();
-//         xvtGraph();
-//         yvtGraph();
-//         xatGraph();
-//         yatGraph();
-//         clearInterval(timer);
-//     }
-//     positionX.push(boxA.position.x);
-//     positionY.push(boxA.position.y);
-//     velocityX.push(boxA.velocity.x);
-//     velocityY.push(boxA.velocity.y);
-//     accelerationX.push((velocityX.at(-1)-velocityX.at(-2))/(fps/1000));
-//     accelerationY.push((velocityY.at(-1)-velocityY.at(-2))/(fps/1000));
-//     time.push(Math.round((time.at(-1)+0.1) * 10) / 10);
-//     console.log(time.at(-1))
-// }, fps)
-
-function xptGraph() {
-    new Chart("xptGraph", {
-        type: "line",
-        data: {
-            labels: time,
-            datasets: [{
-                fill: false,
-                lineTension: 0,
-                data: positionX
-            }]
-        },
-        options: {}
-      });
-}
-
-function yptGraph() {
-    new Chart("yptGraph", {
-        type: "line",
-        data: {
-            labels: time,
-            datasets: [{
-                fill: false,
-                lineTension: 0,
-                data: positionY
-            }]
-        },
-        options: {}
-      });
-}
-
-function xvtGraph() {
-    new Chart("xvtGraph", {
-        type: "line",
-        data: {
-            labels: time,
-            datasets: [{
-                fill: false,
-                lineTension: 0,
-                data: velocityX
-            }]
-        },
-        options: {}
-      });
-}
-
-function yvtGraph() {
-    new Chart("yvtGraph", {
-        type: "line",
-        data: {
-            labels: time,
-            datasets: [{
-                fill: false,
-                lineTension: 0,
-                data: velocityY
-            }]
-        },
-        options: {}
-      });
-}
-
-function xatGraph() {
-    new Chart("xatGraph", {
-        type: "line",
-        data: {
-            labels: time,
-            datasets: [{
-                fill: false,
-                lineTension: 0,
-                data: accelerationX
-            }]
-        },
-        options: {}
-      });
-}
-
-function yatGraph() {
-    new Chart("yatGraph", {
-        type: "line",
-        data: {
-            labels: time,
-            datasets: [{
-                fill: false,
-                lineTension: 0,
-                data: accelerationY
-            }]
-        },
-        options: {}
-      });
-}
-
-
-// function directionVectorY() {
-//     const img = new Image(50, 50);
-//     img.id = "vectorX";
-//     img.src = "./assets/img/vector.png"; 
-//     img.style=`position: absolute; top:${positionY.at(-1)}px; left:${positionX.at(-1)}px`;
-//     document.body.appendChild(img);
-    
-
-// }
